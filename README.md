ad5adbb4a3161ec473e0730fc0229246f99dcf36
Stephanie Hobson <sjhobson@gmail.com>
2018-07-31 21:52:21 -0700

    Add asset pipeline

---
+++ README.md
@@ -0,0 +1,14 @@
+Develop:
+
+`hugo server -wv`
+
+`gulp`
+
+`http://localhost:1313`
+
+Deploy:
+```
+rm -rf public
+hugo
+gulp
+```
---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+var gulp         = require("gulp"),
+    sass         = require("gulp-sass"),
+    hash         = require("gulp-hash"),
+    del          = require("del");
+
+// Compile SCSS files to CSS
+gulp.task("scss", function () {
+    del(["static/css/**/*"]);
+    gulp.src("src/scss/**/*.scss")
+        .pipe(sass({outputStyle : "compressed"}))
+        .pipe(hash())
+        .pipe(gulp.dest("static/css"))
+        //Create a hash map
+        .pipe(hash.manifest("hash.json"))
+        //Put the map in the data directory
+        .pipe(gulp.dest("data/css"));
+});
+
+// Hash images
+gulp.task("images", function () {
+    del(["static/images/**/*"]);
+    gulp.src("src/images/**/*")
+        .pipe(hash())
+        .pipe(gulp.dest("static/images"))
+        .pipe(hash.manifest("hash.json"))
+        .pipe(gulp.dest("data/images"));
+});
+
+// Hash javascript
+gulp.task("js", function () {
+    del(["static/js/**/*"]);
+    gulp.src("src/js/**/*")
+        .pipe(hash())
+        .pipe(gulp.dest("static/js"))
+        .pipe(hash.manifest("hash.json"))
+        .pipe(gulp.dest("data/js"));
+});
+
+// copy icons to public folder
+gulp.task("icons", function() {
+    del(["static/images/icons/*"]);
+    gulp.src([
+        'node_modules/simple-icons/icons/flickr.svg',
+        'node_modules/simple-icons/icons/lanyrd.svg',
+        'node_modules/simple-icons/icons/pinboard.svg',
+        'node_modules/simple-icons/icons/twitter.svg',
+        'node_modules/simple-icons/icons/linkedin.svg',
+        'node_modules/simple-icons/icons/untappd.svg'
+        ])
+        .pipe(gulp.dest('static/images/icons'));
+});
+
+// Watch asset folder for changes
+gulp.task("watch", ["scss", "images", "js", "icons"], function () {
+    gulp.watch("src/scss/**/*", ["scss"]);
+    gulp.watch("src/images/**/*", ["images"]);
+    gulp.watch("src/js/**/*", ["js"]);
+});
+
+// Set watch as default task
+gulp.task("default", ["watch"]);
