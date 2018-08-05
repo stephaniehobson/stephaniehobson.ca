bf67cfe3ed5baabb352ac2139501e161b86ffdf1
Stephanie Hobson <sjhobson@gmail.com>
2018-08-04 19:15:56 -0700

    Update icons in header

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,23 @@
-const gulp = require("gulp");
-const sass = require("gulp-sass");
-const hash = require("gulp-hash");
-const del = require("del");
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const hash = require('gulp-hash');
+const del = require('del');
 const gulpStylelint = require('gulp-stylelint');
 
-// Compile SCSS files to CSS
-gulp.task("scss", function () {
-    del(["static/css/**/*"]);
-    gulp.src("src/scss/main.scss")
-        .pipe(sass({outputStyle : "compressed"}))
+// compile SCSS files to CSS
+gulp.task('scss', () => {
+    del(['static/css/**/*']);
+    gulp.src('src/scss/main.scss')
+        .pipe(sass({outputStyle : 'compressed'}))
         .pipe(hash())
-        .pipe(gulp.dest("static/css"))
+        .pipe(gulp.dest('static/css'))
         //Create a hash map
-        .pipe(hash.manifest("hash.json"))
+        .pipe(hash.manifest('hash.json'))
         //Put the map in the data directory
-        .pipe(gulp.dest("data/css"));
+        .pipe(gulp.dest('data/css'));
 });
 
-
+// lint SCSS files
 gulp.task('scss:lint', () => {
     return gulp.src('src/scss/**/*.scss')
         .pipe(gulpStylelint({
@@ -28,32 +28,32 @@ gulp.task('scss:lint', () => {
         }));
 });
 
-// Hash images
-gulp.task("images", function () {
-    del(["static/images/**/*"]);
-    gulp.src("src/images/**/*")
+// hash images
+gulp.task('images', () => {
+    del(['static/images/**/*']);
+    gulp.src('src/images/**/*')
         .pipe(hash())
-        .pipe(gulp.dest("static/images"))
-        .pipe(hash.manifest("hash.json"))
-        .pipe(gulp.dest("data/images"));
+        .pipe(gulp.dest('static/images'))
+        .pipe(hash.manifest('hash.json'))
+        .pipe(gulp.dest('data/images'));
 });
 
-// Hash javascript
-gulp.task("js", function () {
-    del(["static/js/**/*"]);
-    gulp.src("src/js/**/*")
+// hash javascript
+gulp.task('js', () => {
+    del(['static/js/**/*']);
+    gulp.src('src/js/**/*')
         .pipe(hash())
-        .pipe(gulp.dest("static/js"))
-        .pipe(hash.manifest("hash.json"))
-        .pipe(gulp.dest("data/js"));
+        .pipe(gulp.dest('static/js'))
+        .pipe(hash.manifest('hash.json'))
+        .pipe(gulp.dest('data/js'));
 });
 
 // copy icons to public folder
-gulp.task("icons", function() {
-    del(["static/images/icons/*"]);
+gulp.task('icons', () => {
+    del(['static/images/icons/*']);
     gulp.src([
         'node_modules/simple-icons/icons/flickr.svg',
-        'node_modules/simple-icons/icons/lanyrd.svg',
+        'node_modules/simple-icons/icons/github.svg',
         'node_modules/simple-icons/icons/pinboard.svg',
         'node_modules/simple-icons/icons/twitter.svg',
         'node_modules/simple-icons/icons/linkedin.svg',
@@ -63,11 +63,11 @@ gulp.task("icons", function() {
 });
 
 // Watch asset folder for changes
-gulp.task("watch", ["scss", "scss:lint", "images", "js", "icons"], function () {
-    gulp.watch("src/scss/**/*", ["scss"]);
-    gulp.watch("src/images/**/*", ["images"]);
-    gulp.watch("src/js/**/*", ["js"]);
+gulp.task('watch', ['scss', 'scss:lint', 'images', 'js', 'icons'], () => {
+    gulp.watch('src/scss/**/*', ['scss']);
+    gulp.watch('src/images/**/*', ['images']);
+    gulp.watch('src/js/**/*', ['js']);
 });
 
 // Set watch as default task
-gulp.task("default", ["watch"]);
+gulp.task('default', ['watch']);
