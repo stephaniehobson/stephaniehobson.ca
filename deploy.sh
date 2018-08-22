a175422e3d0a0e3e17b419a65674fec28b9d29e4
Stephanie Hobson <sjhobson@gmail.com>
2018-08-21 23:37:55 -0700

    Change RSS to copy to docs directory

--- deploy.sh
+++ deploy.sh
@@ -15,21 +15,21 @@ echo "CNAME found"
 # make sure wordpress tag feeds continue to work
 # http://stephaniehobson.ca/wordpress/tag/conference/feed/
 for t in bcit conference ; do
-    mkdir -p public/wordpress/tag/$t/feed/
-    ln -s ../../../../tags/$t/index.xml public/wordpress/tag/$t/feed/index.xml
+    mkdir -p docs/wordpress/tag/$t/feed/
+    ln -s ../../../../tags/$t/index.xml docs/wordpress/tag/$t/feed/index.xml
 done
 
 # make sure wordpress category feeds continue to work
 # http://stephaniehobson.ca/wordpress/category/webdesign/feed/
 for c in webdesign personal ; do
-    mkdir -p public/wordpress/category/$c/feed/
-    ln -s ../../../../tags/$c/index.xml public/wordpress/category/$c/feed/index.xml
+    mkdir -p docs/wordpress/category/$c/feed/
+    ln -s ../../../../tags/$c/index.xml docs/wordpress/category/$c/feed/index.xml
 done
 
 # make sure wordpress site feed continues to work
 # http://stephaniehobson.ca/feed/
-mkdir -p public/feed/
-ln -s ../index.xml public/feed/index.xml
+mkdir -p docs/feed/
+ln -s ../index.xml docs/feed/index.xml
 
 git add *
 git commit -m "Generate static site"
